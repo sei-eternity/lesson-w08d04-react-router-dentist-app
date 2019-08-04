4a9b00e6b5f458d1fe8b61e8ab681c950a194801
Marc Wright <marc.wright@gmail.com>
2019-08-04 11:43:08 +0300

    lab complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import Home from './Home'
 import Procedures from './Procedures';
 import Contact from './Contact';
 
+
 export default function App() {
   return (
-    <div>
-      <Home />
-      <Procedures />
-      <Contact />
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>{' '}
+        <Link to="/procedures">Procedures</Link>{' '}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/procedures" component={Procedures} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
   );
 }
