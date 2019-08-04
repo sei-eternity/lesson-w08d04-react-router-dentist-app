56800550870d5490148e9c66bfc1a6ac9ca672f0
Marc Wright <marc.wright@gmail.com>
2019-08-04 11:33:55 +0300

    Part 1 finished

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Dentist Website</h1>
+      <p>
+        Welcome to my dentist website.
+      </p>
+    </div>
+  );
+}
